168d0ff5a777e197064afbeeda5e7d3b3a459e8a
AhadB787 <abhat497@uottawa.ca>
2025-11-26 23:58:03 -0500

    Fixed menu buttons complete

---
+++ Assets/NewEmptyCSharpScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToGNG1103Project : MonoBehaviour
+{
+    public void LoadGNGProject()
+    {
+        SceneManager.LoadScene("GNG 1103 - Project");
+    }
+}
